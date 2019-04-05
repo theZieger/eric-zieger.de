77f78ba73bb37269a9134760fa80a9b6aaddaac1
Eric Zieger <eric.zieger@invia.de>
2019-04-05 17:28:57 +0200

    sw?

---
+++ serviceworker.js
@@ -0,0 +1,57 @@
+const CACHE = 'erics-cache';
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'eric-zieger.jpg',
+                'favicon.ico'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fromCache(event.request).then(
+            function(response) {
+                return response;
+            },
+            function() {
+                return fetch(event.request).then(function(response) {
+                    event.waitUntil(
+                        updateCache(event.request, response.clone())
+                    );
+
+                    return response;
+                });
+            }
+        )
+    );
+});
+
+function fromCache(request) {
+    return caches.open(CACHE).then(function(cache) {
+        return cache.match(request).then(function(matching) {
+            if (!matching || matching.status === 404) {
+                return Promise.reject('no-match');
+            }
+
+            return matching;
+        });
+    });
+}
+
+function updateCache(request, response) {
+    return caches.open(CACHE).then(function(cache) {
+        return cache.put(request, response);
+    });
+}
