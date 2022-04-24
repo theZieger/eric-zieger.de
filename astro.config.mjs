9b70962c478e9a60ae17fbf36379674a12b6f094
Eric Zieger <eric.zieger@camao.one>
2022-04-24 18:47:39 +0200

    fix: building

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: "https://eric-zieger.de", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  renderers: [],
-  markdownOptions: {
+  site: "https://eric-zieger.de", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  markdown: {
     render: ["@astrojs/markdown-remark"],
   },
 };
